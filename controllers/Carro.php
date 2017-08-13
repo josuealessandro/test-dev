594fd776a0725b1e8b4d37220c6e283cb099d182
lucasbsp <30881240+lucasbsp@users.noreply.github.com>
2017-08-13 02:03:04 -0300

    Configuração do Select
    Configuração do select (ajax e parametros da url)

--- controllers/Carro.php
+++ controllers/Carro.php
@@ -6,40 +6,164 @@ namespace controllers {
 
 		//DB Connection
 		function __construct(){
-			$this->PDO = new \PDO('mysql:host=localhost;dbname=api', 'root', '');
+			$this->PDO = new \PDO('mysql:host=localhost;dbname=db.carro', 'root', '');
 			$this->PDO->setAttribute( \PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION );
+		}
+		
+		function verifica_string ($str, $valorPrimario, $valorSecundario): string
+		{		
+			//Busca um valor primario dentro da string. Se encontrar, insere o valor secundario
+			if(strpos($str, $valorPrimario))
+			{
+				$str .= $valorSecundario;
+			}
+			//Se nao encontrar o valor primario dentro da string, insere o valor primario
+			else
+			{
+				$str .= $valorPrimario;
+			}
+			
+			return $str;
 		}
 		
 		//Select
 		public function select($search = null){
 			global $app;
 						
-			//Select all if search is null
+			//Seleciona tudo caso nao haja filtro de busca
 			if($search == null)
 			{
-				$sth = $this->PDO->prepare("SELECT * FROM pessoa");
+				$sth = $this->PDO->prepare("SELECT * FROM TB_CARRO");
 			}
 			else 
 			{
-				if(isset($_GET['id']))
+				//Coloca os parametros da requisicao em caixa baixa para nao haver erros
+				$filter = array_change_key_case($_GET);
+				$sql = "SELECT * FROM TB_CARRO ";
+				
+				//Muda o tipo de busca no banco caso tiver vários filtros
+				if(count($filter) > 1 && !isset($filter['conector']))
 				{
-					//Select by ID
-					$sth = $this->PDO->prepare("SELECT * FROM pessoa WHERE id = :id");
-					$sth ->bindValue(':id',($_GET['id']));					
+					$tipoBusca = "OR";
 				}
-				else if (isset($_GET['nome']))
+				else if(count($filter) > 1 && isset($filter['conector']))
 				{
-					//Select by Name
-					$sth = $this->PDO->prepare("SELECT * FROM pessoa WHERE nome like :nome");
-					$sth ->bindValue(':nome','%'.($_GET['nome']).'%');
+					$tipoBusca = $filter['conector'];
 				}
+				else
+				{
+					$tipoBusca = "";
+				}
+				
+				//Verifica se a requisição possui o parâmetro Id
+				if(isset($filter['id']) && $filter['id'] !== '')
+				{
+					//Seleciona por ID_CARRO
+					$sql = $this->verifica_string($sql, "WHERE", $tipoBusca);
+					$sql .= " ID_CARRO = ".$filter['id']." ";
+				}
+				//Verifica se a requisição possui o parâmetro Marca
+				if (isset($filter['marca']) && $filter['marca'] !== '')
+				{
+					//Seleciona por NOME_MARCA
+					$sql = $this->verifica_string($sql, "WHERE", $tipoBusca);
+					$sql .= " NOME_MARCA LIKE '%".$filter['marca']."%' ";
+				}
+				//Verifica se a requisição possui o parâmetro Modelo
+				if (isset($filter['modelo']) && $filter['modelo'] !== '')
+				{
+					//Seleciona por NOME_MODELO
+					$sql = $this->verifica_string($sql, "WHERE", $tipoBusca);
+					$sql .= " NOME_MODELO LIKE '%".$filter['modelo']."%' ";
+				}
+				//Verifica se a requisição possui o parâmetro Ano
+				if (isset($filter['ano']) && $filter['ano'] !== '')
+				{
+					$anoIni = "0";
+					$anoFim = "0";
+					
+					//A pesquisa pode ser tanto por um ano especifico ou por uma faixa de ano (Ex: de 2015 à 2018)
+					if(strpos($filter['ano'], '-'))
+					{	
+						//Pesquisa por faixa de ano
+						$faixaAno = explode("-", $filter['ano']);						
+					}
+					else
+					{
+						//Pesquisa por ano especifico
+						$faixaAno = $filter['ano'];
+					}
+										
+					if (count($faixaAno) == 1)//Pesquisa por ano especifico
+					{
+						$anoIni = $faixaAno;
+						$anoFim = $anoIni;
+					}
+					else if (count($faixaAno == 2))//Pesquisa por faixa de ano
+					{
+						if($faixaAno[0] < $faixaAno[1])
+						{
+							 $anoIni = $faixaAno[0];
+							 $anoFim = $faixaAno[1];
+						}
+						else
+						{
+							 $anoIni = $faixaAno[1];
+							 $anoFim = $faixaAno[0];
+						}
+					}
+					//Monta a query e substitui os valores
+					$sql = $this->verifica_string($sql, "WHERE", $tipoBusca);
+					$sql .= " ANO >= ".$anoIni." AND ANO <= ".$anoFim." ";
+				}
+				//Configura a ordenacao de exibicao
+				if (isset($filter['orderby']) && $filter['orderby'] !== '')
+				{
+					switch($filter['orderby'])
+					{
+						case "id":
+							$sql .= " ORDER BY ID_CARRO ";
+							break;
+							
+						case "marca":
+							$sql .= " ORDER BY NOME_MARCA ";
+							break;
+							
+						case "modelo":
+							$sql .= " ORDER BY NOME_MODELO ";
+							break;
+							
+						case "ano":
+							$sql .= " ORDER BY ANO ";
+							break;
+						
+						default:
+							$sql .= " ORDER BY ID_CARRO ";
+							break;
+					}
+					// asc = order ascendente / desc = order descendente
+					if (isset($filter['asc']) && $filter['asc'] !== '')
+					{
+						if($filter['asc'] == "true")
+						{
+							$sql .= " ASC ";
+						}
+						else 
+						{ 
+							$sql .= " DESC ";
+						}
+					}
+				}
+				// var_dump($sql);
+				// die();
+				$sth = $this->PDO->prepare($sql);				
 			}
 			if($sth !== "")
 			{
 				$sth->execute();
-				$result = $sth->fetchAll(\PDO::FETCH_ASSOC);				
+				$result = $sth->fetchAll(\PDO::FETCH_ASSOC);
 			}
-			//Return data
+			//Retorna os dados
 			$app->render('default.php',["data"=>$result],200);
 		}
 		
@@ -49,13 +173,13 @@ namespace controllers {
 			$dados = json_decode($app->request->getBody(), true);
 			$dados = (sizeof($dados)==0)? $_POST : $dados;
 			$keys = array_keys($dados); //Paga as chaves do array
-			$sth = $this->PDO->prepare("INSERT INTO pessoa (".implode(',', $keys).") VALUES (:".implode(",:", $keys).")");
+			$sth = $this->PDO->prepare("INSERT INTO TB_CARRO (".implode(',', $keys).") VALUES (:".implode(",:", $keys).")");
 			foreach ($dados as $key => $value) {
 				$sth ->bindValue(':'.$key,$value);
 			}
 			$sth->execute();
-			//Return ID
-			$app->render('default.php',["data"=>['id'=>$this->PDO->lastInsertId()]],200); 
+			//Retorna o ID do carro
+			$app->render('default.php',["data"=>['ID_CARRO'=>$this->PDO->lastInsertId()]],200); 
 		}
 		
 		//Update
@@ -68,21 +192,21 @@ namespace controllers {
 				$sets[] = $key." = :".$key;
 			}
 
-			$sth = $this->PDO->prepare("UPDATE pessoa SET ".implode(',', $sets)." WHERE id = :id");
-			$sth ->bindValue(':id',$id);
+			$sth = $this->PDO->prepare("UPDATE TB_CARRO SET ".implode(',', $sets)." WHERE ID_CARRO = :ID");
+			$sth ->bindValue(':ID',$id);
 			foreach ($dados as $key => $value) {
 				$sth ->bindValue(':'.$key,$value);
 			}
-			//Return status
+			//Retorna status - true = alterado / false = nao alterado 
 			$app->render('default.php',["data"=>['status'=>$sth->execute()==1]],200); 
 		}
 		
 		//Delete
 		public function del($id){
 			global $app;
-			$sth = $this->PDO->prepare("DELETE FROM pessoa WHERE id = :id");
-			$sth ->bindValue(':id',$id);
-			//Return status
+			$sth = $this->PDO->prepare("DELETE FROM TB_CARRO WHERE ID_CARRO = :ID");
+			$sth ->bindValue(':ID',$id);
+			//Retorna status - true = alterado / false = nao alterado 
 			$app->render('default.php',["data"=>['status'=>$sth->execute()==1]],200); 
 		}
 	}
--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function BuscarCarro(str) {
 				if(arrayResult.length !== 0)
 				{
 					$.each(arrayResult,function(i, item){
-						content +='<span id="' + item.id + '">' + item.nome + '<br /></span>';
+						content +='<span id="' + item.ID_CARRO + '">' + item.NOME_MARCA + ' - ' + item.NOME_MODELO + '<br /></span>';
 					});
 				}
 				else { content = ""; }
@@ -27,75 +27,18 @@ function BuscarCarro(str) {
                 document.getElementById("txtBuscaCarro").innerHTML = content;
             }
         };
-        xmlhttp.open("GET", "api.php/carros/search?nome=" + str, true);
+        xmlhttp.open("GET", "api.php/carros/search?marca=" + str + '&modelo=' str, true);
         xmlhttp.send();
     }
 }
 
-function ExcluirCarro(str) {
-    if (str == "") {
-        document.getElementById("txtBuscaCarro").innerHTML = "";
-        return;
-    } else {
-		if (window.XMLHttpRequest) {
-            // code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp = new XMLHttpRequest();
-        } else {
-            // code for IE6, IE5
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-				var arrayResult = jQuery.parseJSON(this.responseText);
-				var content = "";
-				
-				if(arrayResult.length !== 0)
-				{
-					$.each(arrayResult,function(i, item){
-						content +='<span id="' + item.id + '">' + item.nome + '<br /></span>';
-					});
-				}
-				else { content = ""; }
-				
-                document.getElementById("txtBuscaCarro").innerHTML = content;
-            }
-        };
-        xmlhttp.open("DELETE", "api.php/carros/id=" + str, true);
-        xmlhttp.send();
-    }
-}
-
-function ExcluirCarro(str) {
-    if (str == "") {
-        document.getElementById("txtBuscaCarro").innerHTML = "";
-        return;
-    } else {
-		if (window.XMLHttpRequest) {
-            // code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp = new XMLHttpRequest();
-        } else {
-            // code for IE6, IE5
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-				var arrayResult = jQuery.parseJSON(this.responseText);
-				var content = "";
-				
-				if(arrayResult.length !== 0)
-				{
-					$.each(arrayResult,function(i, item){
-						content +='<span id="' + item.id + '">' + item.nome + '<br /></span>';
-					});
-				}
-				else { content = ""; }
-				
-                document.getElementById("txtBuscaCarro").innerHTML = content;
-            }
-        };
-        xmlhttp.open("DELETE", "api.php/carros/id=" + str, true);
-        xmlhttp.send();
-    }
+function CopiarConteudo(IdOrigem, IdDestino)
+{
+	var origem = document.getElementById(IdOrigem.id);
+	var destino = document.getElementById(IdDestino.id);
+	
+	if(origem.innerHTML !== "")
+	{
+		destino.innerHTML = origem.innerHTML;
+	}
 }
